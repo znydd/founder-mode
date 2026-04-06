1adbc0419b0400a966ff51981cffbc5a4b3df9ed
Znyd <mailboxofnabil@gmail.com>
2026-04-07 05:53:42 +0600

    feat: set icon of all size

--- manifest.config.ts
+++ manifest.config.ts
@@ -6,11 +6,17 @@ export default defineManifest({
   name: pkg.name,
   version: pkg.version,
   icons: {
-    48: 'public/logo.png',
+    16: 'public/icon16.png',
+    32: 'public/icon32.png',
+    48: 'public/icon48.png',
+    128: 'public/icon128.png',
   },
   action: {
     default_icon: {
-      48: 'public/logo.png',
+      16: 'public/icon16.png',
+      32: 'public/icon32.png',
+      48: 'public/icon48.png',
+      128: 'public/icon128.png',
     },
     default_popup: 'src/popup/index.html',
   },
